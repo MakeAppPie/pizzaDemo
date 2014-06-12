7fb2fd369e1f092673604562682bc973d6ee64d8
MakeAppPie <shlomo626@gmail.com>
2014-06-12 10:57:56 -0500

    Pizza Demo
    A Apple Swift demo program using UI. See makeAppPie.com for details.

--- pizzaDemo/ViewController.swift
+++ pizzaDemo/ViewController.swift
@@ -8,18 +8,56 @@
 
 import UIKit
 
+
+
+
+
+
+
 class ViewController: UIViewController {
-                            
+    
+    let π=3.1415926
+    let pi=3.1415926
+    let clearString = "I Like Pizza!"
+    let maxPizza = 24.0
+    var z = 0.0
+    var 🍕 = 0.0
+    var diameter = 0.0
+    var area:Double = 0.0
+    
+    
+    @IBOutlet var resultsDisplayLabel : UILabel
+    @IBOutlet var sliderDisplayLabel : UILabel
+    @IBOutlet var diameterSlider : UISlider
+    @IBAction func computeButton(sender : UIButton) {
+        area = pi * z * z
+        let formattedAnswer = NSString(format:"%6.2f",area)
+        let unitsString = "  square in of 🍕"
+        resultsDisplayLabel.text = formattedAnswer + unitsString
+    }
+    @IBAction func clearDisplayButton(sender : UIButton) {
+        resultsDisplayLabel.text = clearString
+        diameterSlider.value = 0
+    }
+    
+    @IBAction func diameterSlider(sender : UISlider) {
+        diameter = Double(sender.value) * maxPizza
+        z = diameter / 2.0
+        let feedbackString = NSString(format:"%6.2f in",diameter)
+        sliderDisplayLabel.text = feedbackString
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        resultsDisplayLabel.text = clearString
+        view.backgroundColor = UIColor(red:0.99,green:0.9,blue:0.9,alpha:1.0)
+        diameterSlider.value = 0.0
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
